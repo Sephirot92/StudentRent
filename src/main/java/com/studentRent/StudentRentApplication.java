d49703887fc163e6a78b49a9b43ed42f833dda12
Sephirot92 <lczapraga@gmail.com>
2020-12-20 17:46:05 +0100

    Initial Commit

---
+++ src/main/java/com/studentRent/StudentRentApplication.java
@@ -0,0 +1,13 @@
+package com.studentRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentRent/StudentRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
